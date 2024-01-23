2d4f610d29f3abbca9a6a6fac834c01813378757
sonw-vh <dibongaodu123@gmail.com>
2024-01-23 09:42:23 +0700

    update

--- views/gioithieu.php
+++ views/gioithieu.php
@@ -4,24 +4,24 @@
     </div>
 
     <div>
-        <div class="row">
+        <div class="post">
             <div class="intro">
                 <!-- Nội dung bài viết -->
                 <h3>✨Chuẩn rong biển Hàn - Tiệm cơm Nori✨</h3>
-                <p>Mấy bà có thắc mắc Nori dùng rong biển gì mà nhập ở đâu không ạ </p>
-                <p>😍 Cùng Nori tìm hiểu nhaa: </p>
-                <p>🍙 Rong biển Nori nhập trực tiếp bên Hàn và giao từ Hàn về đây (Nên lâu lâu Nori nghỉ đợi hàng về á) </p>
-                <p>🍙 Rong biển Nori đảm bảo ngon, "xịn", chuẩn Hàn </p>
-                <p>🍙 Lá rong xanh xanh, rong rất thơm và là hàng "độc quyền" của Nori ạ </p>
-                <p>‼️ Vậy nên ai ăn cơm Nori lâu cũng biết Nori chọn nguyên liệu rất kĩ và đảm bảo chất lượng ✨✨✨ </p>
-                <p>____________________ </p>
-                <p>𝐍𝐡𝐚̣̂𝐧 đ𝐨̛𝐧 𝐭𝐫𝐮̛𝐨̛́𝐜 𝟏 𝐧𝐠𝐚̀𝐲 - 𝐭𝐫𝐮̛𝐨̛́𝐜 𝟑𝟎 𝐩𝐡𝐮́𝐭 𝐭𝐫𝐨𝐧𝐠 𝐧𝐠𝐚̀𝐲 </p>
-                <p>Chuyên nhận đơn số lượng lớn / siêu thị / sự kiện </p>
-                <p>𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: @tiemcom.nori </p>
-                <p>𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤: Tiệm Cơm Nori - Onigiri Đà Nẵng </p>
-                <p>🏠 42/24 Nguyễn Thành Hãn </p>
-                <p>☎️ 0965700799 - 0968120902 </p>
-                <p>⏰ 6:30 - 17:00 | T2 - CN </p>
+                <p style="font-family: Tahoma">Mấy bà có thắc mắc Nori dùng rong biển gì mà nhập ở đâu không ạ </p>
+                <p style="font-family: Tahoma">😍 Cùng Nori tìm hiểu nhaa: </p>
+                <p style="font-family: Tahoma">🍙 Rong biển Nori nhập trực tiếp bên Hàn và giao từ Hàn về đây (Nên lâu lâu Nori nghỉ đợi hàng về á) </p>
+                <p style="font-family: Tahoma">🍙 Rong biển Nori đảm bảo ngon, "xịn", chuẩn Hàn </p>
+                <p style="font-family: Tahoma">🍙 Lá rong xanh xanh, rong rất thơm và là hàng "độc quyền" của Nori ạ </p>
+                <p style="font-family: Tahoma">‼️ Vậy nên ai ăn cơm Nori lâu cũng biết Nori chọn nguyên liệu rất kĩ và đảm bảo chất lượng ✨✨✨ </p>
+                <p style="font-family: Tahoma">____________________ </p>
+                <p style="font-family: Tahoma"><strong>Nhận đơn trước 1 ngày - trước 30 phút trong ngày</strong></p>
+                <p style="font-family: Tahoma">Chuyên nhận đơn số lượng lớn / siêu thị / sự kiện </p>
+                <p style="font-family: Tahoma"><strong>Instagram</strong>: @tiemcom.nori </p>
+                <p style="font-family: Tahoma"><strong>Facebook</strong>: Tiệm Cơm Nori - Onigiri Đà Nẵng </p>
+                <p style="font-family: Tahoma">🏠 42/24 Nguyễn Thành Hãn </p>
+                <p style="font-family: Tahoma">☎️ 0965700799 - 0968120902 </p>
+                <p style="font-family: Tahoma">⏰ 6:30 - 17:00 | T2 - CN </p><p> </p><p> </p>
                 
 
                 <!-- Hình ảnh -->
